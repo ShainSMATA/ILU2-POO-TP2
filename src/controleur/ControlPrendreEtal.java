06c1ef835d9ad3bbe20b3efb5d7dca4733d923a0
ShainSMATA <haroun.shain@gmail.com>
2024-11-07 09:41:24 +0100

    mitp2p

--- src/controleur/ControlPrendreEtal.java
+++ src/controleur/ControlPrendreEtal.java
@@ -1,5 +1,6 @@
 package controleur;
 
+import personnages.Gaulois;
 import villagegaulois.Village;
 
 public class ControlPrendreEtal {
@@ -13,8 +14,11 @@ public class ControlPrendreEtal {
 	}
 
 	public boolean resteEtals() {
-		//TODO a completer, attention le retour ne dit pas etre false :-)
-		return false;
+		
+		return village.rechercherEtalVide();
+		
+		
+		
 	}
 
 	public int prendreEtal(String nomVendeur, String produit, int nbProduit) {
@@ -24,7 +28,9 @@ public class ControlPrendreEtal {
 	}
 
 	public boolean verifierIdentite(String nomVendeur) {
-		//TODO a completer, attention le retour ne dit pas etre false :-)
-		return false;
+		
+		return controlVerifierIdentite.verifierIdentite(nomVendeur);
+		
+
 	}
 }
